fd5b3e685277e79e5a0f99f50c14d7038e898aff
danieaneta <83565907+danieaneta@users.noreply.github.com>
2024-11-08 17:02:40 -0600

    update

--- disparity_smooth.py
+++ disparity_smooth.py
@@ -5,72 +5,31 @@ import cv2
 from PIL import Image
 
 
-'''
-Take a matrix, and select a pixel in the matrix > 1st pixel selection will be (1,1)
-take in surroundings/perimeter >
-    store "perimeter" = [] 
-    left, right, top, bottom = [-1, 0], [1, 0], [0, 1], [0, -1]                
-for pixel in matrix: 
-    if left (-1)is None:
-        append 0 (EMPTY)
-    else:
-        append 1 (NOT-EMPTY)
-        
-    if right (+1) is None:
-        append 0 (EMPTY)
-    else:
-        append 1 (NOT-EMPTY)
-        
-    if top (+1) is None:
-        append 0 (EMPTY)
-    else: 
-        append 1 (NOT-EMPTY)
-        
-    if bottom (-1) is None:
-        append 0 (EMPTY)
-    else:
-        append 1 (NOT-EMPTY)
-            
-ERROR CHECK: check if perimeter list len = 4
-    if not 4: 
-        perimeter = []
-        go back and redo checks 
-    else:
-        continue
-        
-if list has one 0:
-    figure out where zero is
-    process
-if list has 2 zero: 
-    figure out where zeros are
-    process
-    
-    
-    
-    
-create a matrix equal to dimensions of image
-look through perimeter list
-    if list has one 0:
-        figure out where zero is
-        process
-    if list has 2 zero: 
-        figure out where zeros are
-        process
+#large test
+# line_image = np.ones((10, 20, 3), dtype=np.uint8) * 255  # Start with a white image
+#Small test
+# line_image = np.ones((3, 3, 3), dtype=np.uint8) * 255  # Start with a white image
 
-processes 
+grayscale_matrix = np.array([
+    [50, 100, 150],
+    [200, 250, 25],
+    [75, 125, 175]
+], dtype=np.uint8)
 
-'''
 
-line_image = np.ones((10, 20, 3), dtype=np.uint8) * 255  # Start with a white image
-cv2.imwrite('test_white.png', line_image)
+cv2.imwrite('grayscale.png', grayscale_matrix)
 
-image = cv2.imread('test_white.png', cv2.IMREAD_GRAYSCALE)
+
+
+image = cv2.imread('grayscale.png', cv2.IMREAD_GRAYSCALE)
 # image = Image.open('test.jpg')
 
 # print(image[0, 0, 0])
 # print(image[1])
 # print(image[2])
 # print(image)
+
+print(image)
 
 height, width = image.shape[0], image.shape[1]
 total_pixels = height * width
@@ -95,24 +54,132 @@ for i in range(height):
         matrix.remove(i)
 
 # print(matrix_lists[1][0])
+# print("MATRIX_LIST: ", matrix_lists)
 
-print(matrix_lists[0][-1])
+# print(matrix_lists[0][-1])
 
+rand_end = matrix_lists[0][0]
+
+end_points = []
 for c in range(height):
     end_pixel = matrix_lists[c][-1]
+    end_points.append(end_pixel)
+
+if rand_end in end_points:
+    print("present")
+else:
+    print("not present") 
+
+
+#width + height
+
+# for r in width_list:
+#     for c in height_list:
+#         print(matrix_lists[c][r])
+
+# print(matrix_lists[0][0])
+# print(matrix_lists[-1])
+
+
+index = matrix_lists[0][0]
+# print("INDEX: ", index)
+
+index_h = matrix_lists[0][0][0]
+# print(index_h)
+index_v = matrix_lists[0][0][1]
+# print(index_v)
+
+pixel_selection = image[index]
+# print(pixel_selection)
+
+
+image_pixel = image[index_h, index_v]
+# print('IMAGE_PIXEL: ', image_pixel)
+
+h_end_point_list = []
+for i in height_list:
+    end_point = matrix_lists[i][-1]
+    print(end_point)
+    h_end_point_list.append(end_point)
+
+v_end_point_list = matrix_lists[-1]
 
 
 
-# for c in range(height):
-#     for i in range(width):
-#         print(matrix_lists[c][i])
-
- 
+# print(matrix_lists[0][0+1][0])
 
 
-# for i in height_list:
-#     print(i)
+print("MATRIX_LISTS: ", matrix_lists)
 
+print("-------------START-------------")
+
+# h_diff = []
+# for c in height_list:
+#     for r in width_list:
+#         print(c, r)
+#         print('MAIN COORDINATES: ', matrix_lists[c][r])
+#         print('END_POINT_LIST: ', h_end_point_list)
+#         if matrix_lists[c][r] in h_end_point_list:
+#             secondary_index_h, secondary_index_v = matrix_lists[c][r-1][0], matrix_lists[c][r-1][1]
+#             print('SECONDARY COORDINATES: ', [secondary_index_h, secondary_index_v])
+#             secondary_pixel = image[secondary_index_h, secondary_index_v]
+#         else:
+#             secondary_index_h, secondary_index_v = matrix_lists[c][r+1][0], matrix_lists[c][r+1][1]
+#             print('SECONDARY COORDINATES: ', [secondary_index_h, secondary_index_v])
+#             secondary_pixel = image[secondary_index_h, secondary_index_v]
+#         primary_pixel_h, primary_pixel_v = matrix_lists[c][r][0], matrix_lists[c][r][1]
+#         main_pixel = image[primary_pixel_h, primary_pixel_v]
+#         print("SECONDARY_PIXEL: ", secondary_pixel)
+#         print("MAIN_PIXEL: ", main_pixel)
+#         difference = secondary_pixel - main_pixel
+#         print("DIFFERENCE: ", difference)
+#         h_diff.append(difference)
+#         print("---end---")
+
+# print(h_diff)
+
+v_diff = []
+for r in width_list:
+    for c in height_list:
+        print('C_R: ', c, r)
+        print("MAIN COORDINATES: ", matrix_lists[c][r])
+        print('END_POINT_LIST: ', v_end_point_list)
+        if matrix_lists[c][r] in v_end_point_list:
+            secondary_index_h, secondary_index_v = matrix_lists[c-1][r][0], matrix_lists[c-1][r][1]
+            print('SECONDARY COORDINATES: ', [secondary_index_h, secondary_index_v])
+            secondary_pixel = image[secondary_index_h, secondary_index_v]
+        else:
+            print("R: ", r)
+            secondary_index_h, secondary_index_v = matrix_lists[c+1][r][0], matrix_lists[c+1][r][1]
+            print("R: ", r)
+            print('SECONDARY COORDINATES: ', [secondary_index_h, secondary_index_v])
+            secondary_pixel = image[secondary_index_h, secondary_index_v]
+        primary_pixel_h, primary_pixel_v = matrix_lists[c][r][0], matrix_lists[c][r][1]
+        main_pixel = image[primary_pixel_h, primary_pixel_v]
+        print("MAIN_PIXEL_TYPE: ", type(main_pixel))
+        print("SECONDARY_PIXEL: ", secondary_pixel)
+        print("MAIN_PIXEL: ", main_pixel)
+        difference = secondary_pixel - main_pixel
+        print("DIFFERENCE: ", difference)
+        v_diff.append(difference)
+        print("---end---")
+print(v_diff)
+
+# secondary_coordinates = []
+# for r in width_list:
+#     for c in height_list:
+#         if matrix_lists[c][r] in v_end_point_list:
+#             secondary_index_h, secondary_index_v = matrix_lists[c-1][r][0], matrix_lists[c-1][r][1]
+#             secondary_pixel = image[secondary_index_h, secondary_index_v]
+#         else:
+#             secondary_index_h, secondary_index_v = matrix_lists[c+1][r][0], matrix_lists[c+1][r][1]
+#             secondary_pixel = image[secondary_index_h, secondary_index_v]
+
+#         secondary = [secondary_index_h, secondary_index_v]
+#         secondary_coordinates.append(secondary)
+
+
+# print("SECONDARY_COORDINATES: ", secondary_coordinates)
 
 
 # print(height, width)
@@ -151,29 +218,65 @@ class Disparity_Smooth():
                 matrix.remove(i)
         return matrix_master
     
-    def end_pixel_list(self, matrix_master):
-        pass
+    def h_end_pixel_list(self, matrix_master, height_list):
+        end_list = []
+        for i in height_list:
+            end_point = matrix_master[i][-1]
+            end_list.append(end_point)
+        return end_list
 
-    def horizontal_process(self, img, matrix_master, height_list, width_list):
+    def horizontal_diff(self, img, matrix_master, height_list, width_list, h_end_point_list):
         h_diff = []
         for c in height_list:
             for r in width_list:
-                if matrix_master[c][r] == 3:
-                    secondary_pixel = img[matrix_master[c][r-1]]
+                if matrix_master[c][r] in h_end_point_list:
+                    secondary_index_h, secondary_index_v = matrix_master[c][r-1][0], matrix_master[c][r-1][1]
+                    secondary_pixel = img[secondary_index_h, secondary_index_v]
                 else:
-                    secondary_pixel = img[matrix_master[c][r+1]]
-                main_pixel = img[matrix_master[c][r]]
+                    secondary_index_h, secondary_index_v = matrix_master[c][r+1][0], matrix_master[c][r+1][1]
+                    secondary_pixel = img[secondary_index_h, secondary_index_v]
+                primary_pixel_h, primary_pixel_v = matrix_master[c][r][0], matrix_master[c][r][1]
+                main_pixel = img[primary_pixel_h, primary_pixel_v]
                 difference = secondary_pixel - main_pixel
                 h_diff.append(difference)
         return h_diff
 
-    def vertical_proccess(self):
+    def v_end_pixel_list(self, matrix_master):
+        end_list = matrix_master[-1]
+        return end_list
+
+    def vertical_diff(self, img, matrix_master, height_list, width_list, v_end_point_list):
+        v_diff = []
+        for r in width_list:
+            for c in height_list:
+                if matrix_master[c][r] in v_end_point_list:
+                    secondary_index_h, secondary_index_v = matrix_master[c-1][r][0], matrix_master[c-1][r][1]
+                    secondary_pixel = img[secondary_index_h, secondary_index_v]
+                else:
+                    secondary_index_h, secondary_index_v = matrix_master[c+1][r][0], matrix_master[c+1][r][1]
+                    secondary_pixel = img[secondary_index_h, secondary_index_v]
+                primary_pixel_h, primary_pixel_v = matrix_master[c][r][0], matrix_master[c][r][1]
+                main_pixel = img[primary_pixel_h, primary_pixel_v]
+                difference = secondary_pixel - main_pixel
+                v_diff.append(difference)
+        return v_diff
+
+    def h_intensity_diff(self, matrix_master, height_list, width_list, h_end_point_list):
         pass
 
+    def horizontal_term_x(self, matrix_master, h_diff, height_list, width_list):
+        #take h_diff(n,n) x e^-I(n,n)
+        pass
+
+    def vertical_term_y(self):
+        pass
 
     def calculate(self):
         img, shape = self.read_image()
         height, width, height_list, width_list = self.get_perimeter(shape)
         matrix = self.create_index_matrix(height_list, width_list)
         matrix_master = self.organize_matrix_lists(matrix, height_list)
-        h_diff = self.horizontal_process(img, matrix_master, height_list, width_list)
+        h_end_point_list = self.h_end_pixel_list(matrix_master, height_list)
+        h_diff = self.horizontal_diff(img, matrix_master, height_list, width_list, h_end_point_list)
+        v_end_point_list = self.v_end_pixel_list(matrix_master)
+        v_diff = self.vertical_diff(img, matrix_master, height_list, width_list, v_end_point_list)
